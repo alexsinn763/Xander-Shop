7f7e6ada4b02ef9880ef1567fa11b64ca8a5a9ca
P4 <P4@ADRIAN-CPU>
2021-10-01 14:49:09 -0400

    finished it woooooo

--- Shop Moment/Form1.cs
+++ Shop Moment/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+using System.Threading;
 
 namespace Shop_Moment
 {
@@ -64,18 +66,40 @@ namespace Shop_Moment
             }
         }
 
+        private async Task tenderButton_ClickAsync(object sender, EventArgs e)
+        {
+           
+        }
+
         private void tenderButton_Click(object sender, EventArgs e)
         {
             try
             {
                 tender = Convert.ToInt32(tenderInsert.Text);
                 change = total - tender;
+                SoundPlayer printSound = new SoundPlayer(Properties.Resources.cashSound);
+                printSound.Play();
+                Thread.Sleep(500);
+                Refresh();
                 cricketAgain.Text = $"{cricketAmount} boxes purchased. {cricketTotal.ToString("C")}";
+                Thread.Sleep(500);
+                Refresh();
                 maddenAgain.Text = $"{maddenAmount} Maddens purchased. {maddenTotal.ToString("C")}";
+                Thread.Sleep(500);
+                Refresh();
                 bitcoinAgain.Text = $"{bitcoinAmount} fractions purchaed. {bitcoinTotal.ToString("C")}";
+                Thread.Sleep(500);
+                Refresh();
                 permOutput.Text = $"Your total is {total.ToString("C")}.";
+                Thread.Sleep(500);
+                Refresh();
                 permOutput2.Text = $"Your amount paid is {tender.ToString("C")}.";
+                Thread.Sleep(500);
+                Refresh();
                 permOutput3.Text = $"Your change is {change.ToString("C")}.";
+                Thread.Sleep(500);
+                Refresh();
+             
             }
             catch
             {
@@ -85,7 +109,30 @@ namespace Shop_Moment
 
         private void clearButton_Click(object sender, EventArgs e)
         {
+            cricketAmount = 0;
+            maddenAmount = 0;
+            bitcoinAmount = 0;
+            cricketTotal = 0;
+            maddenTotal = 0;
+            bitcoinTotal = 0;
+            tender = 0;
+            change = 0;
+            subTotal = 0;
+            tax2 = 0;
+            total = 0;
+            cricketAgain.Text = " ";
+            maddenAgain.Text = " ";
+            bitcoinAgain.Text = " ";
+            permOutput.Text = " ";
+            permOutput2.Text = " ";
+            permOutput3.Text = " ";
+
             
         }
+
+        //private async Task tenderButton_ClickAsync(object sender, EventArgs e)
+        //{
+
+        //}
     }
 }
